664dbfb03a9c3750fa434d35ffd0a06b99c75cf8
rajendra <rajendrammarboyana@gmail.com>
2024-06-11 13:22:33 +0100

    appuser variable

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,7 @@
 component=$1
+rm -f ~/*.json
+ansible-playbook  get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
 
 
 
-ansible-playbook -i $component-dev.rajdevops.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
+ansible-playbook -i $component-dev.rajdevops.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml -e '@~/secrets.json'
