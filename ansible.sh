e682c79df8b0f2851d343dff6ef547fe5d82cc32
rajendra <rajendrammarboyana@gmail.com>
2024-06-20 11:15:33 +0100

    ins

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,11 @@
 component=$1
+
 rm -f ~/*.json
 ansible-playbook  get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
+#ansible-playbook -i $component-$env.rajdevops.online, -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
 
 
-ansible-playbook -i $component-dev.rajdevops.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml -e '@~/secrets.json'
+aws ec2 describe-instances --filters Name=tag:Name,Values=$component-$env Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
+ansible-playbook -i inv -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
+
 rm -f ~/*.json
